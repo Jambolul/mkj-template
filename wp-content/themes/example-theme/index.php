d670061b47d7edf1693dcd453e2d76d3b5725b8d
jambolul <jami.naatanen@gmail.com>
2024-03-20 15:53:00 +0200

    update

--- wp-content/themes/example-theme/index.php
+++ wp-content/themes/example-theme/index.php
@@ -3,7 +3,7 @@
 if (have_posts()) :
     while (have_posts()) :
         the_post();
-        the_title("<h1>", "</h1>");g
+        the_title("<h1>", "</h1>");
         the_content();
     endwhile;
 else :
