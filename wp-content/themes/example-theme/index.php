5f3e768910719ea0c7f736d3ac0f044c1bace679
jambolul <jami.naatanen@gmail.com>
2024-03-20 15:52:42 +0200

    update

--- wp-content/themes/example-theme/index.php
+++ wp-content/themes/example-theme/index.php
@@ -1,1 +1,12 @@
 <?php
+
+if (have_posts()) :
+    while (have_posts()) :
+        the_post();
+        the_title("<h1>", "</h1>");g
+        the_content();
+    endwhile;
+else :
+    _e('Sorry, no posts matched your criteria.', 'esimerkki');
+endif;
+?>
